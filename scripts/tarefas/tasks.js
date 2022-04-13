db783b94ff665d569e69da055bb5698b7ede24a1
CJBiohacker <cj.moyses@gmail.com>
2022-04-13 00:36:09 -0300

    feat : Função criarTarefa em andamento

--- scripts/tarefas/tasks.js
+++ scripts/tarefas/tasks.js
@@ -1,20 +1,20 @@
 // variaveis
-let nomeUsuario = document.querySelector('.user-info p')
-let token = sessionStorage.getItem('jwt')
+let nomeUsuario = document.querySelector('.user-info p');
+let token = sessionStorage.getItem('jwt');
+let novaTarefa = document.getElementById('novaTarefa');
 
+onload = function () {
+    if (!token) {
+        location.href = 'index.html'
+    } else {
+        pegarUsuario(token)
+    }
 
-onload = function(){
-        if(!token){
-            location.href = 'index.html'
-        }else{
-            pegarUsuario(token)
-        }
-   
 }
 
 
 //função para pegar o nome e sobrenome do usuário a partir do token
-function pegarUsuario(valor){
+function pegarUsuario(valor) {
     let endPoin = {
         method: 'GET',
         headers: {
@@ -25,9 +25,8 @@ function pegarUsuario(valor){
     let url = 'https://ctd-todo-api.herokuapp.com/v1/users/getMe'
 
     fetch(url, endPoin)
-    .then(response => response.json())
-    .then(data => 
-        {
+        .then(response => response.json())
+        .then(data => {
             nomeUsuario.innerHTML = `${data.firstName} ${data.lastName}`
         })
 
@@ -39,4 +38,26 @@ function pegarUsuario(valor){
 
 
 //função para criar uma nova tarefa
+const criarTarefa = (inputTarefa = novaTarefa) => {
 
+    let tarefa = {
+        description: inputTarefa.innerHTML,
+        completed: false
+    };
+
+    let urlCriarTarefa = "https://ctd-todo-api.herokuapp.com/v1/tasks";
+    let novaTarefaJSON = JSON.stringify(tarefa);
+
+    console.log(novaTarefaJSON)
+
+    let endPoint = {
+        method: 'POST',
+        headers: {
+            'content-type': 'application/json'
+        },
+        body: novaTarefaJSON
+    }
+
+    fetch(urlCriarTarefa, endPoint)
+        .then()
+}
