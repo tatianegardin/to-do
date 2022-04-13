ca29c52b0718fd9ce149cf612ee88d829c31d5d6
tatiane gardin <contatotatianegardin@hotmail.com>
2022-04-12 22:56:57 -0300

    novas funcionalidades à página de tarefas

--- scripts/tarefas/tasks.js
+++ scripts/tarefas/tasks.js
@@ -1,8 +1,42 @@
+// variaveis
+let nomeUsuario = document.querySelector('.user-info p')
+let token = sessionStorage.getItem('jwt')
+
+
 onload = function(){
-    let token = sessionStorage.getItem('jwt')
         if(!token){
             location.href = 'index.html'
         }else{
-            console.log(token)
+            pegarUsuario(token)
         }
+   
 }
+
+
+//função para pegar o nome e sobrenome do usuário a partir do token
+function pegarUsuario(valor){
+    let endPoin = {
+        method: 'GET',
+        headers: {
+            authorization: valor
+        }
+    }
+
+    let url = 'https://ctd-todo-api.herokuapp.com/v1/users/getMe'
+
+    fetch(url, endPoin)
+    .then(response => response.json())
+    .then(data => 
+        {
+            nomeUsuario.innerHTML = `${data.firstName} ${data.lastName}`
+        })
+
+}
+
+
+//função para listar as tarefas
+
+
+
+//função para criar uma nova tarefa
+
