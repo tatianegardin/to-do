382e040f0fa26a107f5cd390ba42f151c87ab54d
tatiane gardin <contatotatianegardin@hotmail.com>
2022-04-15 13:17:25 -0300

    tratando erro

--- scripts/tarefas/getUsuario.js
+++ scripts/tarefas/getUsuario.js
@@ -10,9 +10,16 @@ function pegarUsuario(valor) {
     let url = 'https://ctd-todo-api.herokuapp.com/v1/users/getMe'
 
     fetch(url, endPoint)
-        .then(response => response.json())
+        .then(response => {
+            if(response.status == 200){
+                return response.json()
+            }else {
+                throw response.status
+            }
+        })
         .then(data => {
             nomeUsuario.innerHTML = `${data.firstName} ${data.lastName}`
         })
+        .catch(error => Swal.fire('Tente novamente mais tarde', '', 'error'))
 
 }
