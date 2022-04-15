ab64ae56d3e48a94b7a4d12a7e02d2e444e2c48a
CJBiohacker <cj.moyses@gmail.com>
2022-04-14 22:50:46 -0300

    fix : correção função lista de tarefas

--- scripts/tarefas/renderiza/tarefaFinalizada.js
+++ scripts/tarefas/renderiza/tarefaFinalizada.js
@@ -1,18 +1,16 @@
-function tarefaFinalizada(tarefa){
+function tarefaFinalizada(tarefa) {
     let ul = document.querySelector('.tarefas-terminadas')
     let li = document.createElement('li')
     li.classList.add('tarefa')
     li.innerHTML = `
-    <li class="tarefa">
-        <div class="done"></div>
-        <div class="descricao">
-        <p class="nome">${tarefa.description}</p>
-        <div>
-            <button><i id="${tarefa.id}" class="fas fa-undo-alt change"></i></button>
-            <button><i id="${tarefa.id}" class="far fa-trash-alt"></i></button>
-        </div>
-        </div>
-    </li>
+    <div class="done"></div>
+    <div class="descricao">
+    <p class="nome">${tarefa.description}</p>
+    <div>
+        <button><i id="${tarefa.id}" class="fas fa-undo-alt change"></i></button>
+        <button><i id="${tarefa.id}" class="far fa-trash-alt"></i></button>
+    </div>
+    </div>
 `
     ul.appendChild(li)
 
--- scripts/tarefas/renderiza/tarefaPendente.js
+++ scripts/tarefas/renderiza/tarefaPendente.js
@@ -1,16 +1,13 @@
-function tarefaPendente(tarefa){
+function tarefaPendente(tarefa) {
     let ul = document.querySelector('.tarefas-pendentes')
     let li = document.createElement('li')
     li.classList.add('tarefa')
     li.innerHTML = `
-    <li class="tarefa">
-        <div class="not-done" id="${tarefa.id}"></div>
-        <div class="descricao">
-            <p class="nome">${tarefa.description}</p>
-            <p class="timestamp"><i class="far fa-calendar-alt"></i> ${tarefa.createdAt}</p>
-        </div>
-    </li>
-
+    <div class="not-done" id="${tarefa.id}"></div>
+    <div class="descricao">
+        <p class="nome">${tarefa.description}</p>
+        <p class="timestamp"><i class="far fa-calendar-alt"></i> ${tarefa.createdAt}</p>
+    </div>
 `
     ul.appendChild(li)
 
